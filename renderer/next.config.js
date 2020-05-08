37ba798a3c1956ef62bb3c3ed6e374c43aa2792d
Waseem Ahmed <codewaseem@gmail.com>
2020-05-08 16:57:25 +0530

    refactor

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -3,47 +3,56 @@ const withSass = require("@zeit/next-sass");
 const lessToJS = require("less-vars-to-js");
 const fs = require("fs");
 const path = require("path");
+const withPlugins = require("next-compose-plugins");
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
   fs.readFileSync(path.resolve(__dirname, "./less/antd-custom.less"), "utf8")
 );
 
-module.exports = withSass({
-  cssModules: true,
-  ...withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables, // make your antd custom effective
-      importLoaders: 0,
-    },
-    cssLoaderOptions: {
-      importLoaders: 3,
-      localIdentName: "[local]___[hash:base64:5]",
-    },
-    webpack: (config, { isServer }) => {
-      //Make Ant styles work with less
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === "function") {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === "function" ? [] : origExternals),
-        ];
+const nextConfig = {
+  distDir: ".next",
+};
 
-        config.module.rules.unshift({
-          test: antStyles,
-          use: "null-loader",
-        });
-      }
-      return config;
-    },
+const plugins = [
+  withSass({
+    cssModules: true,
+    ...withLess({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables, // make your antd custom effective
+        importLoaders: 0,
+      },
+      cssLoaderOptions: {
+        importLoaders: 3,
+        localIdentName: "[local]___[hash:base64:5]",
+      },
+      webpack: (config, { isServer }) => {
+        //Make Ant styles work with less
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === "function") {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === "function" ? [] : origExternals),
+          ];
+
+          config.module.rules.unshift({
+            test: antStyles,
+            use: "null-loader",
+          });
+        }
+        return config;
+      },
+    }),
   }),
-});
+];
+
+module.exports = withPlugins(plugins, nextConfig);
