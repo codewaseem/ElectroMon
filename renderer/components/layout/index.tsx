71c02b5ec01edd89e666ed5450c58ad504669bf2
Waseem Ahmed <codewaseem@gmail.com>
2020-05-08 14:53:04 +0530

    perfectly working antd with css modules

--- renderer/components/layout/index.tsx
+++ renderer/components/layout/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import styles from "./style.module.css";
+import styles from "./style.module.scss";
 import { Switch } from "antd";
 
 console.log(styles);
 
 export default function Layout() {
   return (
-    <div className={"container"}>
-      {/* <div className={styles.container}> */}
+    <div className={styles.container}>
       <Switch />
     </div>
   );
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,56 +1,49 @@
+const withLess = require("@zeit/next-less");
+const withSass = require("@zeit/next-sass");
 const lessToJS = require("less-vars-to-js");
 const fs = require("fs");
 const path = require("path");
-const process = require("process");
-const withSass = require("@zeit/next-sass");
 
+// Where your antd-custom.less file lives
 const themeVariables = lessToJS(
   fs.readFileSync(path.resolve(__dirname, "./less/antd-custom.less"), "utf8")
 );
 
-const isDev = process.env.NODE_ENV === "development";
-
 module.exports = withSass({
-  cssLoaderOptions: {
-    modules: true,
-    localIdentName: isDev
-      ? "[path][name]__[local]___[hash:base64:6]"
-      : "_[hash:base64:6]",
-  },
-  webpack(config, options) {
-    const { rules } = config.module;
-    const { use } = rules[rules.length - 1];
-    const loaders = options.isServer
-      ? ["css-loader"]
-      : use.slice(0, isDev ? 2 : 1).concat(["css-loader"]);
-
-    config.module.rules.push({
-      test: /.*\.(css|less)$/,
-      use: [
-        ...loaders,
-        {
-          loader: "less-loader",
-          options: {
-            javascriptEnabled: true,
-            modifyVars: themeVariables,
-            sourceMap: isDev,
+  cssModules: true,
+  ...withLess({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables, // make your antd custom effective
+      importLoaders: 0,
+    },
+    cssLoaderOptions: {
+      importLoaders: 3,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
+    webpack: (config, { isServer }) => {
+      //Make Ant styles work with less
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === "function") {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
           },
-        },
-      ],
-    });
+          ...(typeof origExternals[0] === "function" ? [] : origExternals),
+        ];
 
-    if (options.isServer) {
-      const externalsFunc = config.externals[0];
-
-      config.externals[0] = function (context, request, callback) {
-        if (/(antd|rc-|css-animation|@ant-design)/.test(request)) {
-          return callback();
-        }
-
-        return externalsFunc(context, request, callback);
-      };
-    }
-
-    return config;
-  },
+        config.module.rules.unshift({
+          test: antStyles,
+          use: "null-loader",
+        });
+      }
+      return config;
+    },
+  }),
 });
