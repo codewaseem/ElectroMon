750bc2923154baf12193b79f7055b5e5073c1c3f
Waseem Ahmed <codewaseem@gmail.com>
2020-05-08 08:40:44 +0530

    add antd

--- renderer/components/layout/index.tsx
+++ renderer/components/layout/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./style.module.css";
+import { Switch } from "antd";
 
 export default function Layout() {
-  return <div className={styles.container}>layout</div>;
+  return (
+    <div className={styles.container}>
+      <Switch />
+    </div>
+  );
 }
