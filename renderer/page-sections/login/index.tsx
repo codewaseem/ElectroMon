d22615e3a8234cda18806b4f2d26e70a34c5c7cf
Waseem Ahmed <codewaseem@gmail.com>
2020-05-13 11:21:08 +0530

    handle check for user loging locally

--- renderer/page-sections/login/index.tsx
+++ renderer/page-sections/login/index.tsx
@@ -1,10 +1,9 @@
-import { Form, Input, Button, message } from "antd";
+import { Form, Input, Button, message, Spin } from "antd";
 import styles from "./login.module.scss";
 import Logo from "../../components/logo";
 import { LoginOutlined } from "@ant-design/icons";
-import useAsyncCode from "../../hooks/useAsyncCode";
 import { AiMonitorContext } from "../../context";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const layout = {
   labelCol: { span: 8 },
@@ -14,26 +13,43 @@ const tailLayout = {
   wrapperCol: { offset: 12, span: 16 },
 };
 
-const LoginStates = {
-  initial: {
-    submitLoading: false,
-  },
-  logging: {
-    submitLoading: true,
-  },
-  done: {
-    submitLoading: false,
-  },
-  error: {
-    submitLoading: false,
-  },
+const defaultState = {
+  spinLoading: false,
+  submitLoading: false,
 };
 
-const defaultState = LoginStates.initial;
+const LoginStates = {
+  no: {
+    ...defaultState,
+  },
+  initial: {
+    ...defaultState,
+    spinLoading: true,
+  },
+  logging: {
+    ...defaultState,
+    submitLoading: true,
+  },
+  done: defaultState,
+  error: defaultState,
+};
 
 const LoginForm = ({ onComplete }) => {
   const aiMonitorAPI = useContext(AiMonitorContext);
-  const [loginState, setLoginState] = useState(defaultState);
+  const [loginState, setLoginState] = useState(LoginStates.initial);
+
+  useEffect(() => {
+    // loading user from local db
+    let id = setTimeout(() => {
+      // if user found call on complete
+      onComplete();
+
+      //else set next state
+      // setLoginState(LoginStates.no);
+    }, 2000);
+
+    return () => clearTimeout(id);
+  }, []);
 
   const onFinish = (values) => {
     setLoginState(LoginStates.logging);
@@ -54,47 +70,49 @@ const LoginForm = ({ onComplete }) => {
   };
 
   return (
-    <div className={styles.formContainer}>
-      <Logo />
-      <Form
-        labelAlign="left"
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please input your email!" }]}
+    <Spin spinning={loginState.spinLoading}>
+      <div className={styles.formContainer}>
+        <Logo />
+        <Form
+          labelAlign="left"
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
-          <Input type="email" placeholder="john@example.com" />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input type="email" placeholder="john@example.com" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password placeholder="pas5w0rd" />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="pas5w0rd" />
+          </Form.Item>
 
-        {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
         <Checkbox>Remember me</Checkbox>
       </Form.Item> */}
 
-        <Form.Item {...tailLayout}>
-          <Button
-            loading={loginState.submitLoading}
-            type="primary"
-            htmlType="submit"
-          >
-            Login <LoginOutlined />
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item {...tailLayout}>
+            <Button
+              loading={loginState.submitLoading}
+              type="primary"
+              htmlType="submit"
+            >
+              Login <LoginOutlined />
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Spin>
   );
 };
 
