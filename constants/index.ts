d6047606553a6f31d0e1d9f15c08e36b41ca1751
Waseem Ahmed <codewaseem@gmail.com>
2020-05-14 12:46:17 +0530

    update names

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,9 @@
-export const WORK_TIMER = "WORK_TIMER";
-export const LUNCH_TIMER = "LUNCH_TIMER";
-export const COFFEE_TIMER = "COFFEE_TIMER";
+export const WORK_TIMER = "WORK";
+export const LUNCH_TIMER = "LUNCH";
+export const COFFEE_TIMER = "COFFEE";
 
-export const TIMERS_STORAGE_KEY = "timers-data";
-export const TIMERS_HISTORY_KEY = "timers-history";
+export const TIMERS_STORAGE_KEY = "timers--data";
+export const TIMERS_HISTORY_KEY = "logs-history";
 export const TIME_DIFFERENCE_DETECTED = "Time difference detected!";
 
 export const MONITOR_API_URL = "https://pulse-api-dev.apps.recruit.aptask.com/api/v1/apps/monitor"
