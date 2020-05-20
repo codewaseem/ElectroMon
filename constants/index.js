f0cd614be574355d4ed7a9932007e2728b4bda47
codewaseem <codewaseem@gmail.com>
2020-05-20 21:05:50 +0530

    udpate url

--- constants/index.js
+++ constants/index.js
@@ -6,16 +6,13 @@ export const TIMERS_STORAGE_KEY = "timers--data";
 export const TIMERS_HISTORY_KEY = "logs-history";
 export const TIME_DIFFERENCE_DETECTED = "Time difference detected!";
 
-
 export const UPDATER_EVENTS = {
-    CHECK_FOR_UPDATES: 'checking-for-update',
-    UPDATE_AVAILABLE: 'update-available',
-    UPDATE_NOT_AVAILABLE: 'update-not-available',
-    ERROR: 'error',
-    DOWNLOAD_PROGRESS: 'download-progress',
-    UPDATE_DOWNLOADED: 'update-downloaded'
-}
+  CHECK_FOR_UPDATES: "checking-for-update",
+  UPDATE_AVAILABLE: "update-available",
+  UPDATE_NOT_AVAILABLE: "update-not-available",
+  ERROR: "error",
+  DOWNLOAD_PROGRESS: "download-progress",
+  UPDATE_DOWNLOADED: "update-downloaded",
+};
 
-
-
-export const MONITOR_API_URL = "https://pulse-api-dev.apps.recruit.aptask.com/api/v1/apps/monitor"
+export const MONITOR_API_URL = "https://api.dev.aptask.com/api/v1/apps/monitor";
