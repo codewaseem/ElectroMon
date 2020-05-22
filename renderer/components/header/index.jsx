439f88fd545ea479bd3505aba99085506ca78f8d
codewaseem <codewaseem@gmail.com>
2020-05-22 22:14:59 +0530

    add logout button

--- renderer/components/header/index.jsx
+++ renderer/components/header/index.jsx
@@ -1,16 +1,38 @@
-import { PageHeader, Avatar } from "antd";
+import { PageHeader, Avatar, Button } from "antd";
 import styles from "./header.module.scss";
 import React from "react";
 import Logo from "../logo";
 import TimeInfo from "./TimeInfo";
 import { AUTH_DATA_KEY } from "../../../constants";
+import { useLogout } from "../../hooks/useMainProcess";
 
 export default function AppHeader() {
   const userInfo = JSON.parse(localStorage.getItem(AUTH_DATA_KEY)).userInfo;
   const extras = [];
+  const logout = useLogout();
+
+  async function handleLogoutClick() {
+    await logout();
+    console.log("logout done!");
+  }
 
   if (userInfo && userInfo.picture) {
-    extras.push(<Avatar key="avatar" size="large" src={userInfo.picture} />);
+    extras.push(
+      <div key="avatar">
+        <Button
+          style={{
+            marginRight: 10,
+          }}
+          size="small"
+          danger
+          htmlType="button"
+          onClick={handleLogoutClick}
+        >
+          Log out
+        </Button>
+        <Avatar size="large" src={userInfo.picture} />
+      </div>
+    );
   }
 
   return (
